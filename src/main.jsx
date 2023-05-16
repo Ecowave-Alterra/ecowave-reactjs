d69f8f089c98f8164d242d1ef7e058adddbf5867
TanIvanAgustian <111202012471@mhs.dinus.ac.id>
2023-05-16 14:42:30 +0700

    input library routing & tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route >
+        <Route path="/" element={<App />} />
+        {/* <Route path="/" element={<LandingPage />} />
+        <Route path="/admin" element={<AdminRoot/>} >
+          <Route path="dashboard" element={<Dashboard />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="" element={<NotFound />} /> */}
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
