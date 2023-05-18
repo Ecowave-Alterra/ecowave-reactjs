a577260d56e4673e0ae450131045499322ee22e8
alwanar21 <alwanar210801@gmail.com>
2023-05-19 00:30:23 +0700

    feat : menambahkan routing dan pages components

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,46 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Login from './components/login'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    RouterProvider,
 } from "react-router-dom";
 
+//layout
+import AdminRoot from "./pages/admin/AdminRoot";
+
+//pages
+import Login from "./pages/Login";
+import LandingPage from "./pages/LandingPage";
+import NotFound from "./pages/NotFound";
+
+import Dashboard from "./pages/admin/dashboard/Dashboard";
+import Pesanan from "./pages/admin/pesanan/Pesanan";
+import Ulasan from "./pages/admin/ulasan/Ulasan";
+import Informasi from "./pages/admin/dataInduk/informasi/Informasi";
+import Product from "./pages/admin/dataInduk/produk/Product";
+
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route >
-        <Route path="/" element={<Login/>} />
-        {/* <Route path="/" element={<LandingPage />} />
-        <Route path="/admin" element={<AdminRoot/>} >
-          <Route path="dashboard" element={<Dashboard />} />
+    createRoutesFromElements(
+        <Route>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/admin" element={<AdminRoot />}>
+                <Route path="dashboard" element={<Dashboard />} />
+                <Route path="informasi" element={<Informasi />} />
+                <Route path="product" element={<Product />} />
+                <Route path="pesanan" element={<Pesanan />} />
+                <Route path="ulasan" element={<Ulasan />} />
+            </Route>
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<NotFound />} />
         </Route>
-        <Route path="/login" element={<Login />} />
-        <Route path="" element={<NotFound />} /> */}
-    </Route>
-  )
+    )
 );
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
