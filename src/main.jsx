598fd6fec73ad721324786d748be36e449c8dc29
alwanar21 <alwanar210801@gmail.com>
2023-05-31 20:28:42 +0700

    feat:menambah beberapa component

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from 'react-router-dom';
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    RouterProvider,
+} from "react-router-dom";
 
 //layout
-import AdminRoot from './routes/AdminRoot';
+import AdminRoot from "./routes/AdminRoot";
 
 //pages
-import Login from './pages/Login';
-import LandingPage from './pages/LandingPage';
-import NotFound from './pages/NotFound';
-
+import Login from "./pages/Login";
+import LandingPage from "./pages/LandingPage";
+import NotFound from "./pages/NotFound";
 
 import Dashboard from "./pages/admin/dashboard";
 import Ulasan from "./pages/admin/ulasan";
 
 // Pesanan
 import Pesanan from "./pages/admin/pesanan";
-import DetailPesanan from './pages/admin/pesanan/DetailPesanan';
+import DetailPesanan from "./pages/admin/pesanan/DetailPesanan";
 
 //informasi
 import Informasi from "./pages/admin/dataInduk/informasi";
@@ -35,6 +34,8 @@ import Produk from "./pages/admin/dataInduk/produk";
 import TambahProduk from "./pages/admin/dataInduk/produk/TambahProduk";
 import UbahProduk from "./pages/admin/dataInduk/produk/UbahProduk";
 
+import Kategori from "./pages/admin/dataInduk/kategori";
+
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route>
@@ -45,6 +46,7 @@ const router = createBrowserRouter(
                 <Route path="informasi/tambah" element={<TambahInformasi />} />
                 <Route path="informasi/ubah" element={<UbahInformasi />} />
                 <Route path="informasi/:id" element={<DetailInformasi />} />
+                <Route path="kategori" element={<Kategori />} />
                 <Route path="produk" element={<Produk />} />
                 <Route path="produk/tambah" element={<TambahProduk />} />
                 <Route path="produk/ubah" element={<UbahProduk />} />
@@ -58,8 +60,8 @@ const router = createBrowserRouter(
     )
 );
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
--- src/routes/AdminRoot.jsx
+++ src/routes/AdminRoot.jsx
@@ -1,14 +1,12 @@
-import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 
-
 export default function AdminRoot() {
-  return (
-    <div>
-      <Sidebar>
-        <Outlet/>
-      </Sidebar>
-    </div>
-  )
+    return (
+        <div>
+            <Sidebar>
+                <Outlet />
+            </Sidebar>
+        </div>
+    );
 }
