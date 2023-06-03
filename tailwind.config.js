c667e17c3cdd0fce4680a84065a9cb1479e0df78
alwanar21 <alwanar210801@gmail.com>
2023-06-03 17:47:31 +0700

    feat:menambah beberapa component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+        "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+    ],
     theme: {
         colors: {
             transparent: "transparent",
@@ -47,5 +51,5 @@ export default {
 
         extend: {},
     },
-    plugins: [],
+    plugins: [require("flowbite/plugin")],
 };
