7e1d28cc6d5ece21b3285ff7eac2f3f1408fa3a5
Renaldo <renaldo.surya@ti.ukdw.ac.id>
2023-06-14 13:26:59 +0700

    add: menambahkan komponen pagination dan empty data tabel

--- src/pages/admin/ulasan/DetailUlasan.jsx
+++ src/pages/admin/ulasan/DetailUlasan.jsx
@@ -1,69 +1,30 @@
 import React, { useState, useEffect } from "react";
-import Back from "../../../assets/img/Vector.png";
-import { Link } from "react-router-dom";
+import { ChevronLeftIcon } from "@heroicons/react/24/outline";
+import { Link, useParams } from "react-router-dom";
 import { UserIcon } from "@heroicons/react/24/solid";
 import CardReview from "../../../components/CardReview";
+import Pagination from "../../../components/Pagination";
+
+import useCrud from "../../../hooks/FetchMockServer";
 
 function DetailUlasan() {
-  const [itemList, setItemList] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const recordPerPage = 3;
-  const lastIndex = currentPage * recordPerPage;
-  const firstIndex = lastIndex - recordPerPage;
-  const records = itemList.slice(firstIndex, lastIndex);
-  const nPage = Math.ceil(itemList.length / recordPerPage);
-  const numbers = [...Array(nPage + 1).keys()].slice(1);
+  let { reviewId } = useParams();
 
-  const data = [
-    {
-      nomer: "01032412123",
-      produk: "Totebag Tas Belanja Serbaguna",
-      reviewer: { nama: "Shinta Rahma", img: <UserIcon className="w-5 h-5" /> },
-      rating: 4.5,
-      imgs: ["/src/assets/img/media.png", "/src/assets/img/media.png"],
-      komentar: "sangat bagus sesuai gambar tapi lama nyampenyaa :)",
-    },
-    {
-      nomer: "01032412123",
-      produk: "Sendok",
-      reviewer: { nama: "Renaldo", img: <UserIcon className="w-5 h-5" /> },
-      rating: 4.0,
-      imgs: ["/src/assets/img/media.png", "/src/assets/img/media.png"],
-      komentar: "sangat bagus",
-    },
-    {
-      nomer: "01032412123",
-      produk: "Sendok",
-      reviewer: { nama: "Renaldo", img: <UserIcon className="w-5 h-5" /> },
-      rating: 4.0,
-      imgs: ["/src/assets/img/media.png", "/src/assets/img/media.png"],
-      komentar: "sangat bagus",
-    },
-    {
-      nomer: "01032412123",
-      produk: "Sendok",
-      reviewer: { nama: "Renaldo", img: <UserIcon className="w-5 h-5" /> },
-      rating: 4.0,
-      imgs: ["/src/assets/img/media.png", "/src/assets/img/media.png"],
-      komentar: "sangat bagus",
-    },
-    {
-      nomer: "01032412123",
-      produk: "Sendok",
-      reviewer: { nama: "Renaldo", img: <UserIcon className="w-5 h-5" /> },
-      rating: 4.0,
-      imgs: ["/src/assets/img/media.png", "/src/assets/img/media.png"],
-      komentar: "sangat bagus",
-    },
-  ];
-  const columns = [
+  const swrKey = `/admin/reviews/${reviewId}`;
+
+  const { data, isLoading, error } = useCrud(swrKey);
+  if (error) return <div>error</div>;
+  if (isLoading) return <div>loading</div>;
+  console.log(data);
+
+  const TABLE_COLUMS = [
     { header: "Konten" },
     { header: "Rating" },
     { header: "Nama Produk" },
   ];
 
-   // Fungsi untuk pagination
-   const prevPage = () => {
+  // Fungsi untuk pagination
+  const prevPage = () => {
     if (currentPage !== firstIndex) {
       setCurrentPage(currentPage - 1);
     }
@@ -77,96 +38,60 @@ function DetailUlasan() {
     setCurrentPage(id);
   };
 
-  useEffect(() => {
-    setItemList(data);
-  }, []);
-
   return (
     <div className="flex-row px-3 py-8">
       {/* header */}
       <div className="flex justify-start items-center">
         <Link to={"/admin/ulasan"}>
-          <img src={Back} className="h-5 mr-5 cursor-pointer" />
+          <ChevronLeftIcon className="w-5 h-5 text-green-500 cursor-pointer mr-4" />
         </Link>
         <div className="text-h4 font-normal">Detail Ulasan</div>
       </div>
 
-      <div className="relative overflow-x-auto border-[1px] border-gray-500 rounded-md mt-8">
-        <table className="w-full text-left table-auto ">
-          <thead className="text-p3 text-white bg-green-500 ">
+      <div></div>
+      <div className="overflow-x-auto mt-8">
+        <table className="w-full min-w-[1000px] text-p4 text-left text-black border-x-[1px] border-black">
+          <thead className="text-p3 text-white bg-green-500">
             <tr>
-              {columns &&
-                columns.map((head) => (
+              {TABLE_COLUMS &&
+                TABLE_COLUMS.map((head) => (
                   <th className="py-[14px] px-[10px] text-p2 font-medium text-center">
                     {head.header}
                   </th>
                 ))}
             </tr>
           </thead>
-          <tbody>
-            {itemList &&
-              records.map((item) => (
-                <CardReview
-                  key={item.nomer}
-                  produk={item.produk}
-                  reviewer={item.reviewer}
-                  nomer={item.nomer}
-                  rating={item.rating}
-                  imgs={item.imgs}
-                  komentar={item.komentar}
-                />
-              ))}
-          </tbody>
-        </table>
-
-        {/* Pagination */}
-        {records.length >= 1 && (
-          <div className="flex justify-between w-full py-4">
-            <div>
-              <p className="text-p2 font-normal px-5 py-3 text-gray-500">{`Halaman ${currentPage} dari ${nPage}`}</p>
-            </div>
-            <nav>
-              <ul className="list-style-none flex">
-                <li>
-                  <a
-                    className={`cursor-pointer relative block px-5 py-3 text-p2 font-semibold  ${
-                      currentPage === 1 ? "text-gray-300" : "text-green-500"
-                    }`}
-                    onClick={currentPage === 1 ? null : prevPage}
-                  >
-                    Previous
-                  </a>
-                </li>
-                {numbers.map((n, i) => (
-                  <li key={i}>
-                    <p
-                      className={`cursor-pointer relative block px-5 py-3 text-p2 font-semibold rounded-full text-green-500 ${
-                        currentPage === n
-                          ? "bg-green-500 text-white"
-                          : "bg-green-50"
-                      }`}
-                      onClick={() => changePage(n)}
-                    >
-                      {n}
-                    </p>
-                  </li>
+          {isLoading ? (
+            <tr>loading</tr>
+          ) : (
+            <tbody>
+              {data && data.Status == 200 &&
+                data.Reviews.map((review, i) => (
+                  <tr>{review.TransactionId}</tr>
+                  // <CardReview
+                  //   key={review.TransactionId+i}
+                  //   produk={review.ProductName}
+                  //   reviewer="Orang Ganteng"
+                  //   nomer={review.TransactionId}
+                  //   rating={review.Rating}
+                  //   // imgs={<UserIcon />}
+                  //   komentar={review.CommentUser}
+                  // />
                 ))}
-
-                <li>
-                  <a
-                    className={`cursor-pointer relative block px-5 py-3 text-p2 font-semibold  ${
-                      currentPage === nPage ? "text-gray-300" : "text-green-500"
-                    }`}
-                    onClick={currentPage === nPage ? null : nextPage}
-                  >
-                    Next
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        )}
+            </tbody>
+          )}
+        </table>
       </div>
+      {/* Pagination */}
+      {data && (
+        <Pagination
+          currentPage={data.Page}
+          totalPage={data.TotalPage}
+          onPrev={prevPage}
+          onChange={changePage}
+          onNext={nextPage}
+        />
+      )}
     </div>
   );
 }
