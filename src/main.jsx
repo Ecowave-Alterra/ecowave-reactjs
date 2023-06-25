bcd8295fd7bbbd25e9dd771d251940c4dc33262b
Renaldo <renaldo.surya@ti.ukdw.ac.id>
2023-06-25 23:36:11 +0700

    fix: memperbaiki main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,53 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from 'react-router-dom';
-import LoggedIn, { LoggedOut } from './utils/privateRoute';
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    RouterProvider,
+} from "react-router-dom";
+import LoggedIn, { LoggedOut } from "./utils/privateRoute";
 
 //layout
-import AdminRoot from './routes/AdminRoot';
+import AdminRoot from "./routes/AdminRoot";
 
 //pages
-import Login from './pages/Login';
-import LandingPage from './pages/LandingPage';
-import NotFound from './pages/NotFound';
+import Login from "./pages/Login";
+import LandingPage from "./pages/LandingPage";
+import NotFound from "./pages/NotFound";
 
 // Dashboard & Ulasan
-import Dashboard from './pages/admin/dashboard';
-import Ulasan from './pages/admin/ulasan';
+import Dashboard from "./pages/admin/dashboard";
+import Ulasan from "./pages/admin/ulasan";
 
 // Pesanan
-import Pesanan from './pages/admin/pesanan';
-import DetailPesanan from './pages/admin/pesanan/DetailPesanan';
+import Pesanan from "./pages/admin/pesanan";
+import DetailPesanan from "./pages/admin/pesanan/DetailPesanan";
 
 //informasi
-import Informasi from './pages/admin/dataInduk/informasi';
-import TambahInformasi from './pages/admin/dataInduk/informasi/TambahInformasi';
-import UbahInformasi from './pages/admin/dataInduk/informasi/UbahInformasi';
-import DetailInformasi from './pages/admin/dataInduk/informasi/DetailInformasi';
+import Informasi from "./pages/admin/dataInduk/informasi";
+import TambahInformasi from "./pages/admin/dataInduk/informasi/TambahInformasi";
+import UbahInformasi from "./pages/admin/dataInduk/informasi/UbahInformasi";
+import DetailInformasi from "./pages/admin/dataInduk/informasi/DetailInformasi";
 
 //Produk
-import Produk from './pages/admin/dataInduk/produk';
-import TambahProduk from './pages/admin/dataInduk/produk/TambahProduk';
-import UbahProduk from './pages/admin/dataInduk/produk/UbahProduk';
-import DetailUlasan from './pages/admin/ulasan/DetailUlasan';
+import Produk from "./pages/admin/dataInduk/produk";
+import TambahProduk from "./pages/admin/dataInduk/produk/TambahProduk";
+import UbahProduk from "./pages/admin/dataInduk/produk/UbahProduk";
+import DetailProduk from "./pages/admin/dataInduk/produk/detailProduk";
+import DetailUlasan from "./pages/admin/ulasan/DetailUlasan";
 
 // Pengiriman
-import OpsiPengiriman from './pages/admin/dataInduk/opsi';
+import OpsiPengiriman from "./pages/admin/dataInduk/opsi";
 
 // Voucher & Kategori
-import Kategori from './pages/admin/dataInduk/kategori';
-import Voucher from './pages/admin/dataInduk/voucher';
-import TambahVoucher from './pages/admin/dataInduk/voucher/TambahVoucher';
-import UbahVoucher from './pages/admin/dataInduk/voucher/UbahVoucher';
+import Kategori from "./pages/admin/dataInduk/kategori";
+import Voucher from "./pages/admin/dataInduk/voucher";
+import TambahVoucher from "./pages/admin/dataInduk/voucher/TambahVoucher";
+import UbahVoucher from "./pages/admin/dataInduk/voucher/UbahVoucher";
 
 //Metode Pembayaran
-import MetodePembayaran from './pages/admin/dataInduk/metodePembayaran';
+import MetodePembayaran from "./pages/admin/dataInduk/metodePembayaran";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -67,7 +68,8 @@ const router = createBrowserRouter(
           {/* Route data induk */}
           <Route path="produk" element={<Produk />} />
           <Route path="produk/tambah" element={<TambahProduk />} />
-          <Route path="produk/ubah" element={<UbahProduk />} />
+          <Route path="produk/ubah/:productId" element={<UbahProduk />} />
+          <Route path="produk/:productId" element={<DetailProduk />} />
           <Route path="informasi" element={<Informasi />} />
           <Route path="informasi/tambah" element={<TambahInformasi />} />
           <Route path="informasi/ubah/:userId" element={<UbahInformasi />} />
@@ -75,8 +77,8 @@ const router = createBrowserRouter(
           <Route path="opsi" element={<OpsiPengiriman />} />
           <Route path="voucher" element={<Voucher />} />
           <Route path="voucher/tambah" element={<TambahVoucher />} />
-          <Route path="voucher/ubah" element={<UbahVoucher />} />
-          <Route path="metode-pembayaran" element={<MetodePembayaran />} />        
+          <Route path="voucher/ubah/:voucherId" element={<UbahVoucher />} />
+          <Route path="metode-pembayaran" element={<MetodePembayaran />} />
           <Route path="kategori" element={<Kategori />} />
         </Route>
       </Route>
@@ -86,7 +88,7 @@ const router = createBrowserRouter(
   )
 );
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
